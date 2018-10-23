c50b9927ec0baacace065a9c080d8cd9224ffce1
Sebastian Klein <sebastian@sklein-medien.de>
2018-10-23 21:10:54 +0200

    [TASK] Wrap API functions in call_user_func() with a closure function

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,8 +1,12 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'basetemplate8',
-    'Configuration/TypoScript',
-    'Website Templates'
-);
+call_user_func(function () {
+    $extensionKey = 'basetemplate8';
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        $extensionKey,
+        'Configuration/TypoScript',
+        'Website Templates'
+    );
+});
