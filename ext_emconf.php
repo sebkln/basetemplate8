af10d3232179ead648e3eeb1e33d369b019583c1
Sebastian Klein <sebastian@sklein-medien.de>
2018-10-26 21:20:45 +0200

    [FEATURE] Rework BackendLayouts
    This starter kit now provides five BackendLayouts with
    corresponding Fluid templates.
    
    Templates with multiple columns have additional full-width
    columns on the top and bottom to be more flexible.
    
    The example stylesheet contains some declarations from the
    inuitcss framework just to provide a working columns layout
    in the frontend. Of course you'll have to replace this with
    your own stylesheet (and templates).

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Template Foundation TYPO3 v8 LTS',
     'description' => 'Use it as a base for your website configuration. Add all your Stylesheets, JavaScripts and Templates.',
-    'version' => '1.0.2',
+    'version' => '1.0.3',
     'state' => 'stable',
     'category' => 'templates',
     'author' => 'Sebastian Klein',
